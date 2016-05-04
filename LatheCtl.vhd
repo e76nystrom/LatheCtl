ff3bc8ba559e433cc1f01d35e7b1cdca0309acac
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-04 19:24:56 -0400

    "LatheCtl.vhd Modified on 5/4/2016 at 7:24:54 PM"

--- LatheCtl.vhd
+++ LatheCtl.vhd
@@ -917,8 +917,8 @@ begin
       outReg <= (out_bits-1 downto opb => '0') & dspReg;
 
      when XRDSR =>
-      outReg <= (out_bits-1 downto 4 => '0') &
-                zStart & dbgDone & xDoneInt & zDoneInt;
+      outReg <= (out_bits-1 downto 5 => '0') &
+                xStart & zStart & dbgDone & xDoneInt & zDoneInt;
 
      when others =>
       outReg <= x"55aa55aa";
