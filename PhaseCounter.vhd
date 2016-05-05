b8bb5cfee35f81a6cbcdd7b69ca4287803a063d7
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-05 19:22:10 -0400

    "PhaseCounter.vhd Modified on 5/5/2016 at 7:22:09 PM"

--- PhaseCounter.vhd
+++ PhaseCounter.vhd
@@ -58,6 +58,9 @@ architecture Behavioral of PhaseCounter is
    data : inout unsigned (n-1 downto 0));
  end component;
 
+ type fsm is (idle, upd_output);
+ signal state : fsm;
+
  signal last_syn : std_logic_vector(1 downto 0);
 
  signal phasectr : unsigned(phase_bits-1 downto 0); --current phase
@@ -78,47 +81,100 @@ begin
 
  phase_ctr: process(clk)
  begin
-  if (rising_edge(clk)) then           --if clock active
-   last_syn <= last_syn(0) & sync;
-   if (init = '1') then                 --if time to load
+  if (rising_edge(clk)) then
+   if (load = '1') then                 --if load
+    state <= idle;                      --set sart state
     phasectr <= (phase_bits-1 downto 0 => '0');
     totphase <= (tot_bits-1 downto 0 => '0');
    else
-    if (ch = '1') then                  --if clock active and change
-     --if (run_sync = '1') then           --if synchronized mode
-      totphase <= totphase + 1;         --count total phase
-     --end if;
+    if (ch = '1') then                  --if clock
+     case state is
+      when idle =>
+       last_syn <= last_syn(0) & sync;
+       if (run_sync ='1') then
+        state <= upd_sync;
+       else
+        state <= upd_phase;
+       end if;
 
-     if (dir = '1') then                --if encoder turning forward
-      if (phasectr = phaseval) then     --if at maximum count
+      when upd_sync =>
+       totphase <= totphase + 1;
+       state <= upd_phase;
+
+      when upd_phase =>
+       if (dir = '1') then
+        if (phasectr = phaseval) then
+         state <= clr_phase;
+        else
+         state <= inc_phase;
+        end if;
+       else
+        if (phasectr = 0) then
+         state <= set_phase;
+        else
+         state <= dec_phase;
+        end if;
+       end if;
+
+      when inc_phase =>
+       phasectr <= phase_ctr + 1;
+
+      when dec__phase =>
+       phasectr <= phase_ctr - 1;
+
+      when clr_phase =>
        phasectr <= (phase_bits-1 downto 0 => '0'); --reset to zero
-       sync_out <= '1';                 --output sync pulse
-      else                              --if not at maximum
-       phasectr <= phasectr + 1;        --increment counter
-       sync_out <= '0';                 --clear sync pulse
-      end if;
-     else                               --if encoder turning backwards
-      if (phasectr = 0) then            --if at minimum
-       phasectr <= phaseval;            --reset to maximum
-       sync_out <= '1';                 --ouput sync pulse
-      else                              --if not at minimum
-       phasectr <= phasectr - 1;        --count down
-       sync_out <= '0';                 --reset sync pulse
-      end if;
-     end if;
+
+      when set_phase =>
+       phasectr <= phaseval;
+     end case;
     end if;
    end if;
   end if;
  end process;
+ --phase_ctr: process(clk)
+ --begin
+ -- if (rising_edge(clk)) then           --if clock active
+ --  last_syn <= last_syn(0) & sync;
+ --  if (init = '1') then                 --if time to load
+ --   phasectr <= (phase_bits-1 downto 0 => '0');
+ --   totphase <= (tot_bits-1 downto 0 => '0');
+ --  else
+ --   if (ch = '1') then                  --if clock active and change
+ --    if (run_sync = '1') then           --if synchronized mode
+ --     totphase <= totphase + 1;         --count total phase
+ --    end if;
+
+ --    if (dir = '1') then                --if encoder turning forward
+ --     if (phasectr = phaseval) then     --if at maximum count
+ --      phasectr <= (phase_bits-1 downto 0 => '0'); --reset to zero
+ --      sync_out <= '1';                 --output sync pulse
+ --     else                              --if not at maximum
+ --      phasectr <= phasectr + 1;        --increment counter
+ --      sync_out <= '0';                 --clear sync pulse
+ --     end if;
+ --    else                               --if encoder turning backwards
+ --     if (phasectr = 0) then            --if at minimum
+ --      phasectr <= phaseval;            --reset to maximum
+ --      sync_out <= '1';                 --ouput sync pulse
+ --     else                              --if not at minimum
+ --      phasectr <= phasectr - 1;        --count down
+ --      sync_out <= '0';                 --reset sync pulse
+ --     end if;
+ --    end if;
+ --   end if;
+ --  end if;
+ -- end if;
+ --end process;
 
  -- latch phase counter on external sync pulse
 
  syn_change: process(clk)
  begin
   if (rising_edge(clk)) then            --if clock active and change
-   --if ((last_syn(0) = '1') and (last_syn(1) = '0')) then --if rising edge
+   if ((last_syn(0) = '1') and (last_syn(1) = '0')) then --if rising edge
     phasesyn <= phasectr;               --save phase counter
-   --end if;
+   end if;
   end if;
  end process;
 
