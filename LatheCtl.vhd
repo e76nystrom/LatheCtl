6693abdf62c8e3fa7ff3ba85f91e0f200243a64d
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-05 10:26:12 -0400

    "LatheCtl.vhd Modified on 5/5/2016 at 10:26:11 AM"

--- LatheCtl.vhd
+++ LatheCtl.vhd
@@ -1109,7 +1109,7 @@ begin
 
  -- phase counter
 
- phase_sel <= '1' when ((op = XLDPHASE) and (dshift = '1') else '0';
+ phase_sel <= '1' when (op = XLDPHASE) else '0';
  runSync <= '1' when (dbgRSyn = '1') else
             '1' when ((zRunning = '1') and (zSrcSyn = '1')) else
             '0';
