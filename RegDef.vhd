9d4f2da5502ffdf3e11c5dfde95037d7e22d1583
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-06 09:53:11 -0400

    "RegDef.vhd Modified on 5/6/2016 at 9:53:08 AM"

--- RegDef.vhd
+++ RegDef.vhd
@@ -22,67 +22,67 @@ constant XREADREG     : unsigned(opb-1 downto 0) := x"07"; -- read register
 
 constant XRDSR        : unsigned(opb-1 downto 0) := x"08"; -- read status register
 
--- read phase
-
-constant XRDPSYN      : unsigned(opb-1 downto 0) := x"09"; -- read sync phase val
-constant XRDTPHS      : unsigned(opb-1 downto 0) := x"0a"; -- read tot phase val
-
 -- phase counter
 
-constant XLDPHASE     : unsigned(opb-1 downto 0) := x"0b"; -- load phase max
+constant XLDPHASE     : unsigned(opb-1 downto 0) := x"09"; -- load phase max
 
 -- load z motion
 
-constant XLDZFREQ     : unsigned(opb-1 downto 0) := x"0c"; -- load z frequency
-constant XLDZD        : unsigned(opb-1 downto 0) := x"0d"; -- load z initial d
-constant XLDZINCR1    : unsigned(opb-1 downto 0) := x"0e"; -- load z incr1
-constant XLDZINCR2    : unsigned(opb-1 downto 0) := x"0f"; -- load z incr2
-constant XLDZACCEL    : unsigned(opb-1 downto 0) := x"10"; -- load z syn accel
-constant XLDZACLCNT   : unsigned(opb-1 downto 0) := x"11"; -- load z syn acl cnt
-constant XLDZDIST     : unsigned(opb-1 downto 0) := x"12"; -- load z distance
-constant XLDZLOC      : unsigned(opb-1 downto 0) := x"13"; -- load z location
+constant XLDZFREQ     : unsigned(opb-1 downto 0) := x"0a"; -- load z frequency
+constant XLDZD        : unsigned(opb-1 downto 0) := x"0b"; -- load z initial d
+constant XLDZINCR1    : unsigned(opb-1 downto 0) := x"0c"; -- load z incr1
+constant XLDZINCR2    : unsigned(opb-1 downto 0) := x"0d"; -- load z incr2
+constant XLDZACCEL    : unsigned(opb-1 downto 0) := x"0e"; -- load z syn accel
+constant XLDZACLCNT   : unsigned(opb-1 downto 0) := x"0f"; -- load z syn acl cnt
+constant XLDZDIST     : unsigned(opb-1 downto 0) := x"10"; -- load z distance
+constant XLDZLOC      : unsigned(opb-1 downto 0) := x"11"; -- load z location
 
 -- load x motion
 
-constant XLDXFREQ     : unsigned(opb-1 downto 0) := x"14"; -- load x frequency
-constant XLDXD        : unsigned(opb-1 downto 0) := x"15"; -- load x initial d
-constant XLDXINCR1    : unsigned(opb-1 downto 0) := x"16"; -- load x incr1
-constant XLDXINCR2    : unsigned(opb-1 downto 0) := x"17"; -- load x incr2
-constant XLDXACCEL    : unsigned(opb-1 downto 0) := x"18"; -- load x syn accel
-constant XLDXACLCNT   : unsigned(opb-1 downto 0) := x"19"; -- load x syn acl cnt
-constant XLDXDIST     : unsigned(opb-1 downto 0) := x"1a"; -- load x distance
-constant XLDXLOC      : unsigned(opb-1 downto 0) := x"1b"; -- load x location
+constant XLDXFREQ     : unsigned(opb-1 downto 0) := x"12"; -- load x frequency
+constant XLDXD        : unsigned(opb-1 downto 0) := x"13"; -- load x initial d
+constant XLDXINCR1    : unsigned(opb-1 downto 0) := x"14"; -- load x incr1
+constant XLDXINCR2    : unsigned(opb-1 downto 0) := x"15"; -- load x incr2
+constant XLDXACCEL    : unsigned(opb-1 downto 0) := x"16"; -- load x syn accel
+constant XLDXACLCNT   : unsigned(opb-1 downto 0) := x"17"; -- load x syn acl cnt
+constant XLDXDIST     : unsigned(opb-1 downto 0) := x"18"; -- load x distance
+constant XLDXLOC      : unsigned(opb-1 downto 0) := x"19"; -- load x location
 
 -- read z motion
 
-constant XRDZSUM      : unsigned(opb-1 downto 0) := x"1c"; -- read z sync sum
-constant XRDZXPOS     : unsigned(opb-1 downto 0) := x"1d"; -- read z sync x pos
-constant XRDZYPOS     : unsigned(opb-1 downto 0) := x"1e"; -- read z sync y pos
-constant XRDZACLSUM   : unsigned(opb-1 downto 0) := x"1f"; -- read z acl sum
-constant XRDZASTP     : unsigned(opb-1 downto 0) := x"20"; -- read z acl stps
+constant XRDZSUM      : unsigned(opb-1 downto 0) := x"1a"; -- read z sync sum
+constant XRDZXPOS     : unsigned(opb-1 downto 0) := x"1b"; -- read z sync x pos
+constant XRDZYPOS     : unsigned(opb-1 downto 0) := x"1c"; -- read z sync y pos
+constant XRDZACLSUM   : unsigned(opb-1 downto 0) := x"1d"; -- read z acl sum
+constant XRDZASTP     : unsigned(opb-1 downto 0) := x"1e"; -- read z acl stps
 
 -- read x motion
 
-constant XRDXSUM      : unsigned(opb-1 downto 0) := x"21"; -- read x sync sum
-constant XRDXXPOS     : unsigned(opb-1 downto 0) := x"22"; -- read x sync x pos
-constant XRDXYPOS     : unsigned(opb-1 downto 0) := x"23"; -- read x sync y pos
-constant XRDXACLSUM   : unsigned(opb-1 downto 0) := x"24"; -- read x acl sum
-constant XRDXASTP     : unsigned(opb-1 downto 0) := x"25"; -- read z acl stps
+constant XRDXSUM      : unsigned(opb-1 downto 0) := x"1f"; -- read x sync sum
+constant XRDXXPOS     : unsigned(opb-1 downto 0) := x"20"; -- read x sync x pos
+constant XRDXYPOS     : unsigned(opb-1 downto 0) := x"21"; -- read x sync y pos
+constant XRDXACLSUM   : unsigned(opb-1 downto 0) := x"22"; -- read x acl sum
+constant XRDXASTP     : unsigned(opb-1 downto 0) := x"23"; -- read z acl stps
 
 -- read distance
 
-constant XRDZDIST     : unsigned(opb-1 downto 0) := x"26"; -- read z distance
-constant XRDXDIST     : unsigned(opb-1 downto 0) := x"27"; -- read x distance
+constant XRDZDIST     : unsigned(opb-1 downto 0) := x"24"; -- read z distance
+constant XRDXDIST     : unsigned(opb-1 downto 0) := x"25"; -- read x distance
 
 -- read location
 
-constant XRDZLOC      : unsigned(opb-1 downto 0) := x"28"; -- read z location
-constant XRDXLOC      : unsigned(opb-1 downto 0) := x"29"; -- read x location
+constant XRDZLOC      : unsigned(opb-1 downto 0) := x"26"; -- read z location
+constant XRDXLOC      : unsigned(opb-1 downto 0) := x"27"; -- read x location
 
 -- read frequency and state
 
-constant XRDFREQ      : unsigned(opb-1 downto 0) := x"2a"; -- read encoder freq
-constant XRDSTATE     : unsigned(opb-1 downto 0) := x"2b"; -- read state info
+constant XRDFREQ      : unsigned(opb-1 downto 0) := x"28"; -- read encoder freq
+constant XRDSTATE     : unsigned(opb-1 downto 0) := x"29"; -- read state info
+
+-- read phase
+
+constant XRDPSYN      : unsigned(opb-1 downto 0) := x"2a"; -- read sync phase val
+constant XRDTPHS      : unsigned(opb-1 downto 0) := x"2b"; -- read tot phase val
 
 -- phase limit info
 
