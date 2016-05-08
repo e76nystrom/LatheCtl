072bf6fd5184206edc7ef72a284195b5f055f4c5
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-08 08:59:05 -0400

    "FreqCounter.vhd Modified on 5/8/2016 at 8:59:04 AM"

--- FreqCounter.vhd
+++ FreqCounter.vhd
@@ -32,9 +32,11 @@ use IEEE.NUMERIC_STD.ALL;
 entity FreqCounter is
  generic(freq_bits : positive);
  port ( clk : in std_logic;
+        init : in std_logic;
         ch : in std_logic;
         tick : in std_logic;
-        freqCtr_reg : out unsigned(freq_bits-1 downto 0)
+        freqCtr_reg : out unsigned(freq_bits-1 downto 0);
+        ready : out std_logic
         );
 end FreqCounter;
 
@@ -42,18 +44,28 @@ architecture Behavioral of FreqCounter is
 
  signal counter :
   unsigned(freq_bits-1 downto 0) := (freq_bits-1 downto 0 => '0');
+ signal flag : std_logic;
 
 begin
 
  freq_ctr: process(clk)
  begin
-  if (rising_edge(clk)) then           --if clock active
-   if (tick = '1') then                --if ten ms tick
-    freqCtr_reg <= counter;            --send count to output register
-    counter <= (freq_bits-1 downto 0 => '0'); --reset counter
+  if (rising_edge(clk)) then            --if clock active
+   if (init = '1') then                 --if time to init
+    ready <= '0';                       --clear ready flag
+    flag <= '0';                        --clear read flago
    else
-    if (ch = '1') then                  --if phase input change
-     counter <= counter + 1;            --update counter
+    if (tick = '1') then                --if ten ms tick
+     ready <= flag;                     --set ready flag
+     flag <= '1';                       --set flag
+    else
+     if (ch = '1') then                 --if phase input change
+      if (flag = '1') then              --if time for reading
+       freqCtr_reg <= counter;          --copy to output
+       counter <= (freq_bits-1 downto 0 => '0'); --reset counter
+      else
+       counter <= counter + 1;          --update counter
+     end if;
     end if;
    end if;
   end if;
