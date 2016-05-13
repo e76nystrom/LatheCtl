0903137586e8b4fe5e7c33c044450ee50d15139f
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-13 17:15:20 -0400

    "SPI.vhd Modified on 5/13/2016 at 5:15:18 PM"

--- SPI.vhd
+++ SPI.vhd
@@ -44,27 +44,27 @@ end SPI;
 
 architecture Behavioral of SPI is
 
- component ClockEnable is
-  Port ( clk : in  std_logic;
-         ena : in  std_logic;
-         clkena : out std_logic);
- end component;
+ --component ClockEnable is
+ -- Port ( clk : in  std_logic;
+ --        ena : in  std_logic;
+ --        clkena : out std_logic);
+ --end component;
 
- type spi_fsm is (start, idle, active, check_count, copy_reg);
+ type spi_fsm is (start, idle, active, check_count, copy_reg, dclk_wait);
  signal state : spi_fsm := start;
 
  signal count : unsigned(3 downto 0) := "0000";
  signal opReg : unsigned(op_bits-1 downto 0); --op code
 
- signal clkena : std_logic;
+ --signal clkena : std_logic;
 
 begin
 
- clk_ena: ClockEnable
-  port map (
-   clk => clk,
-   ena => dclk,
-   clkena =>clkena);
+ --clk_ena: ClockEnable
+ -- port map (
+ --  clk => clk,
+ --  ena => dclk,
+ --  clkena =>clkena);
  
  din_proc: process(clk)
  begin
@@ -92,13 +92,15 @@ begin
      else
       shift <= '0';
       copy <= '0';
-      if (clkena = '1') then
+      --if (clkena = '1') then
+      if (dclk == '1') then
        if (count /= x"0") then
         opReg <= opReg(op_bits-2 downto 0) & din;
         count <= count - 1;
         state <= check_count;
        else
         shift <= '1';
+        state <= dclk_wait;
        end if;
       end if;
      end if;
@@ -108,12 +110,21 @@ begin
       op <= opReg;
       state <= copy_reg;
      else
-      state <= active;
+      --state <= active;
+      state <= dclk_wait;
      end if;
 
     when copy_reg =>
      copy <= '1';
-     state <= active;
+     --state <= active;
+     state <= dclk_wait;
+
+    when dclk_wait =>
+     shift <= '0';
+     copy <= '0';
+     if (dclk = '0') then
+      state <= active;
+     end if;
  
    end case;
   end if;
