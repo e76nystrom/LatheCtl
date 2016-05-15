b19472536ee49ec869defa0a60558c087c06608b
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-15 07:45:46 -0400

    "ClockEna1.vhd Modified on 5/15/2016 at 7:45:44 AM"

--- ClockEna1.vhd
+++ ClockEna1.vhd
@@ -37,6 +37,7 @@ architecture Behavioral of ClockEnable1 is
 
  constant delayLen : positive := n*2;
  signal clkdly : std_logic_vector(delayLen-1 downto 0);
+ constant dlyVal =
 
 begin
 
@@ -48,11 +49,10 @@ begin
   end if;
  end process;
 
- clkena <= '1' when ((clkdly(n-1 downto 0) = (n-1 downto 0 => '1')) and
-                     (clkdly(delayLen-1 downto n) = (n-1 downto 0 => '0')))
+ clkena <= '1' when clkdly = ((n-1 downto 0 => '0') & (n-1 downto 0 => '1'))
            else '0';
 
 
- clkena <= clkdly(0) and clkdly(1) and not clkdly(2) and not clkdly(3);
+ --clkena <= clkdly(0) and clkdly(1) and not clkdly(2) and not clkdly(3);
 
 end Behavioral;
