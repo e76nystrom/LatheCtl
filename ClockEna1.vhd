5196c99b45b60f74c4d2623965b386959eddf83b
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-15 07:50:58 -0400

    "ClockEna1.vhd Modified on 5/15/2016 at 7:50:57 AM"

--- ClockEna1.vhd
+++ ClockEna1.vhd
@@ -44,14 +44,10 @@ begin
  begin
   if (rising_edge(clk)) then
    clkdly <= clkdly(delayLen-2 downto 0) & ena;
-   --clkcly <= clkdly(2 downto 0) & ena;
   end if;
  end process;
 
  clkena <= '1' when clkdly = ((n-1 downto 0 => '0') & (n-1 downto 0 => '1'))
            else '0';
-
-
- --clkena <= clkdly(0) and clkdly(1) and not clkdly(2) and not clkdly(3);
 
 end Behavioral;
